77d8db1e260ddcc7506c364a9bedccaf74e4778c
Thomas E Cowern <thomas.e.cowern@gmail.com>
2019-01-08 19:34:07 -0700

    updated SDTCalc

---
+++ Week 6/Tuesday/SDTCalculator/SDTController.swift
@@ -0,0 +1,18 @@
+//
+//  SDTController.swift
+//  SDTCalculator
+//
+//  Created by Thomas Cowern New on 1/8/19.
+//  Copyright © 2019 Thomas Cowern New. All rights reserved.
+//
+
+import Foundation
+
+class SDTController {
+    
+    // MARK: - Singleton
+    static let shared = SDTController()
+    
+    // Propterties
+    var results: [String] = []
+}
