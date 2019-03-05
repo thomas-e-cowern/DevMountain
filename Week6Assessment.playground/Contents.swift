053e561f494d2499ecd7a2eeccb042f4b49a0e77
Thomas E Cowern <thomas.e.cowern@gmail.com>
2019-03-05 12:41:48 -0500

    updated sorting algorythm

--- Week6Assessment.playground/Contents.swift
+++ Week6Assessment.playground/Contents.swift
@@ -4,7 +4,7 @@ var names = ["Anne", "Bob", "Charlie", "Deborah", "Evan", "Francine", "Gerry", "
 
 func shuffleNames (names: [String]) -> [String] {
     var names = names
-    var shuffled = [String]();
+    var shuffled = [String]()
     for _ in 0..<names.count {
         let rand = Int(arc4random_uniform(UInt32(names.count)))
         shuffled.append(names[rand])
@@ -14,3 +14,19 @@ func shuffleNames (names: [String]) -> [String] {
 }
 
 print(shuffleNames(names: names))
+
+func groupByTwos (names: [String]) {
+    var names = names
+    var grouped = [[String]]()
+    var newArray = [String]()
+    for _ in 0..<names.count {
+        if names.count >= 2 {
+            for y in 0..<2 {
+                newArray.append(names[y])
+            }
+            grouped.append(newArray)
+        } else {
+            
+        }
+    }
+}
