0f05108eca4600284278e3e0265b322757f7d10b
Thomas E Cowern <thomas.e.cowern@gmail.com>
2019-03-06 15:00:18 -0500

    added grouping to names array

--- Week6Assessment.playground/Contents.swift
+++ Week6Assessment.playground/Contents.swift
@@ -1,6 +1,6 @@
-import UIKit
+import Foundation
 
-var names = ["Anne", "Bob", "Charlie", "Deborah", "Evan", "Francine", "Gerry", "Hilary"]
+var names = ["Anne", "Bob", "Charlie", "Deborah", "Evan", "Francine", "Gerry", "Hilary", "Steve"]
 
 func shuffleNames (names: [String]) -> [String] {
     var names = names
@@ -15,18 +15,37 @@ func shuffleNames (names: [String]) -> [String] {
 
 print(shuffleNames(names: names))
 
-func groupByTwos (names: [String]) {
+func groupByTwos (names: [String])  -> [[String]]{
+    let count = names.count / 2
+    print("Start count: \(count)")
     var names = names
     var grouped = [[String]]()
     var newArray = [String]()
-    for _ in 0..<names.count {
-        if names.count >= 2 {
-            for y in 0..<2 {
-                newArray.append(names[y])
-            }
-            grouped.append(newArray)
+    for _ in 0..<count + 1 {
+        if names.count == 1 {
+            print("should be one")
+            newArray.append(names[0])
+            names.remove(at: 0)
+            print(names)
+        } else if names.count <= 0{
+            print("Zero or less")
         } else {
-            
+            for _ in 0...1 {
+                newArray.append(names[0])
+                names.remove(at: 0)
+            }
         }
+        print(newArray)
+        print(count)
+        grouped.append(newArray)
+        print(grouped)
+        newArray = []
     }
+    print(grouped)
+    return grouped
 }
+
+groupByTwos(names: names)
+print(names)
+names.append("Jamie")
+groupByTwos(names: names)
